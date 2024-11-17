7df16da46b1323f997bb8b9377de0ccad66a32c1
Bodomrus70@mail.ru <Bodomrus70@mail.ru>
2024-11-17 13:14:53 +0700

    Добавьте файлы проекта.

---
+++ Module-25/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿using Module_25.Repositories;
+
+namespace Module_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AppContext())
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
